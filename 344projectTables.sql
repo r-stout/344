385d6ccf3b91eb7f0a6f54d47f81d1ab809056e4
OrganizedCha0s <38442036+OrganizedCha0s@users.noreply.github.com>
2018-04-16 16:44:38 -0500

    Update 344projectTables.sql

--- 344projectTables.sql
+++ 344projectTables.sql
@@ -14,9 +14,10 @@ perdob DATE,
 pereth VARCHAR2 (35),
 pergen VARCHAR2 (1),
 perema VARCHAR2 (35),
-)
-;
--- STUDENT TABLE
+
+CONSTRAINT per_perID_pk PRIMARY KEY(perID);
+
+  -- STUDENT TABLE
 CREATE TABLE stu (
 perID
 stulunch VARCHAR2 (1)
@@ -26,12 +27,33 @@ stugradecomm VARCHAR2 (1)
 stugradesatis VARCHAR2 (1)
 stuhomewrk VARCHAR2 (35)
 stuserviceID VARCHA2 (35) --CHECK THIS
-
+  
+CONSTRAINT stu_perID_pk PRIMARY KEY(perID),
+CONSTRAINT stu_perID_fk FOREIGN KEY (perID) REFERENCES per(perID),
+CONSTRAINT stu_servID_fk FOREIGN KEY (servID) REFERENCES serv(servID)); -- MAKE THIS TABLE
 
 -- PARENT TABLE
-CREATE TABLE par
+CREATE TABLE par (
 stuID VARCHAR2 (35)
 emgcont VARCHAR2 (35)
 parempplace VARCHAR2 (35)
 parrelation VARCHAR2 (35)
 parwrkphn NUMBER (10) --CHECK THIS
+ 
+  
+-- PHONE TABLE  
+CREATE TABLE phn (
+phnID VARCHAR2 (35)
+phntype VARCHAR2 (1)
+phncntry NUMBER (1)
+phnarea NUMBER (1)
+phnexc NUMBER (35)
+
+CONSTRAINT phn_phnID_pk PRIMARY KEY(phnID)) ;
+
+-- INCOME TABLE  
+
+-- SPECIAL SERVICES TABLE
+CREATE TABLE serv (
+  
+
